f63bd700066476d97cb2ed76cc0f9d8a2c300471
Age Brouwer <stage1@marbleautomation.com>
2025-02-12 11:10:40 +0100

    mini fix missed export

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'contact', component: ContactPageComponent },
