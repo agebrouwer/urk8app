e65ac623182f36270a95dd3328625e33acafe817
Age Brouwer <stage1@marbleautomation.com>
2025-02-12 11:08:09 +0100

    some routing fixes I hope

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'contact', component: ContactPageComponent },
-];
+  { path: '**', redirectTo: 'home' },
+]
+
